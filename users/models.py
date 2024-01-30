a3090390a8102fe38000731bb0ae24a327f32985
Noah <76827471+Noah-droid@users.noreply.github.com>
2024-01-30 03:52:32 +0100

    Update models.py

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    username = None
+    username = models.CharField(max_length=255, unique=True)
 
     ROLE_CHOICES = [
         ('student', 'Student'),
@@ -13,3 +13,13 @@ class User(AbstractUser):
     ]
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
 
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    # instructor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses')
+
+class CourseRequest(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='course_requests')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
