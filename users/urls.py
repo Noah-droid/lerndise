d453be41a1d331ce35d82bb43e6b1495cde1c647
Noah <76827471+Noah-droid@users.noreply.github.com>
2024-01-30 03:50:49 +0100

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from .views import *
+from .views import CourseListAPIView, CourseRequestAPIView
 
 urlpatterns = [
     path('register/', registerAPIView.as_view()),
     path('login/', LoginAPIView.as_view()),
     path('user/', UserView.as_view()),
-    path('logout/', LogoutView.as_view())
+    path('logout/', LogoutView.as_view()),
+    path('courses/', CourseListAPIView.as_view(), name='course-list'),
+    path('course-request/', CourseRequestAPIView.as_view(), name='course-request'),
 ]
