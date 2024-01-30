55197b78f4a867d7e4b9fc6cf2aee961cb6fa009
Noah <76827471+Noah-droid@users.noreply.github.com>
2024-01-30 03:54:27 +0100

    Update asgi.py

--- lerndise/asgi.py
+++ lerndise/asgi.py
@@ -4,6 +4,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authtutorial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lerndise.settings')
 
 application = get_asgi_application()
