4341fdc31ad261b36c8d5817efc25d8b88e85631
Noah <76827471+Noah-droid@users.noreply.github.com>
2024-02-07 09:23:01 +0100

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+
+admin.site.register(CourseRequest)
+admin.site.register(Course)
+admin.site.register(User)
